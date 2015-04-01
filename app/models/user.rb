d5163d55f6e4fc3cd8a898ed420207e97217da55
Caius Mersa <caiusmersa@gmail.com>
2015-04-01 14:25:39 -0700

    Redo first half of Chapter 11

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 #Why do we create an attr)accessor for activation_token but not for activation_digest
 
-  class User < ActiveRecord::Base
+class User < ActiveRecord::Base
+  has_many :microposts, dependent: :destroy
   attr_accessor :remember_token, :activation_token, :reset_token
   before_save   :downcase_email
   before_create :create_activation_digest
