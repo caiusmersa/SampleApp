de06a50046f16569db77f76a7c20928cfc5e0a8e
Caius Mersa <caiusmersa@gmail.com>
2015-04-09 13:42:45 -0700

    Correct the carrier wave initializer

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -3,10 +3,10 @@ if Rails.env.production?
     config.fog_credentials = {
       # Configuration for Amazon S3
       :provider              => 'AWS',
-      :aws_access_key_id     => ENV['AKIAJTNTL34RKBMG4HXQ'],
-      :aws_secret_access_key => ENV['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']
+      :aws_access_key_id     => ENV['S3_ACCESS_KEY'],
+      :aws_secret_access_key => ENV['S3_SECRET_KEY']
     }
-    config.fog_directory     =  ENV['fotosforsampleapp']
+    config.fog_directory     =  ENV['S3_BUCKET']
   end
 end
 
